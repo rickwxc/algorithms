b1f0a0943c730222bbce6af0307af390f656626f
Rick Wang <lurker2000wxc@gmail.com>
2018-11-01 11:17:25 +1100

    dfs

---
+++ graph/dfs.rb
@@ -0,0 +1,14 @@
+require '../node'
+
+
+g = Graph.new(4)
+
+g.add_edge(0, 1); 
+g.add_edge(0, 2); 
+g.add_edge(1, 2); 
+g.add_edge(2, 0); 
+g.add_edge(2, 3); 
+g.add_edge(3, 3); 
+
+visited = []
+p g.dfs(2, visited)
