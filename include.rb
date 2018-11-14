a5c0edabaf4fd5dfe8c3d8298de092c8d696dc41
Rick Wang <lurker2000wxc@gmail.com>
2018-11-15 09:15:50 +1100

    node clean

--- include.rb
+++ include.rb
@@ -1,8 +1,8 @@
-$LOAD_PATH << File.join(File.dirname(__FILE__), "/utils")
+$LOAD_PATH << File.join(File.dirname(__FILE__), '/utils')
 
-require "undirect_graph.rb"
-require "direct_graph.rb"
-require "node.rb"
-require "weighted_undirect_graph.rb"
+require 'undirect_graph.rb'
+require 'direct_graph.rb'
+require 'node.rb'
+require 'weighted_undirect_graph.rb'
 
-require "pry"
+require 'pry'
